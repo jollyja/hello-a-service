471becaf9bae550856f2509cd8dd8643c29ecc7d
王培嘉 <jollyja@gmail.com>
2021-03-17 13:33:58 +0800

    test

---
+++ src/main/java/cn/hello/a/controller/AController.java
@@ -0,0 +1,22 @@
+package cn.hello.a.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class AController {
+
+    @PostMapping(path = "/a/start")
+    public Map<String, String> done(@RequestBody Map<String, String> req) {
+        Map<String, String> res = new HashMap<>();
+        res.put("abc", "def");
+        return res;
+    }
+
+
+
+}
---
+++ src/test/java/cn/hello/a/AApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hello.a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
