e537b9a755da49dc1a7685d1515feb16e2086618
王培嘉 <jollyja@gmail.com>
2021-03-29 19:27:39 +0800

    test

--- src/main/java/cn/hello/a/controller/AController.java
+++ src/main/java/cn/hello/a/controller/AController.java
@@ -13,7 +13,6 @@ public class AController {
     @PostMapping(path = "/a/start")
     public Map<String, String> done(@RequestBody Map<String, String> req) {
         Map<String, String> res = new HashMap<>();
-        res.put(null, "1");
         res.put("abc", "def");
         return res;
     }
